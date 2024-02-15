882d7db46bdd8790613e5f8bf7b6e09061ac6780
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-02-15 09:39:04 +0100

    test: Add UnitTesting for API

---
+++ test/NotificationHub.API.Test/Controllers/NotificationControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NotificationHub.API.Controllers;
+using NotificationHub.API.Test.Mocks;
+using NotificationHub.Application;
+
+namespace NotificationHub.API.Test.Controllers;
+
+public class NotificationControllerTests
+{
+    private const string host = "http://localhost:5188";
+    private const string pathBase = "/api";
+
+    public NotificationControllerTests() { }
+
+    [Theory]
+    [InlineData(NotificationType.Email_SMTP)]
+    [InlineData(NotificationType.Email_O365)]
+    [InlineData(NotificationType.SMS)]
+    [InlineData(NotificationType.WhatsApp)]
+    public async Task SendNotification_ShouldReturnOk_IfAllParamsAreCorrect(NotificationType notificationType)
+    {
+        // Arrange
+        var request = MockHttpRequest.GetMock(host, pathBase);
+
+        var context = Mock.Of<HttpContext>(_ =>
+            _.Request == request.Object
+        );
+
+        var controllerContext = new ControllerContext()
+        {
+            HttpContext = context
+        };
+
+        var logger = new Mock<ILogger<NotificationController>>();
+        var notificationHub = new Mock<INotificationHub>();
+
+        var controller = new NotificationController(logger: logger.Object,
+            notificationHub: notificationHub.Object)
+        {
+            ControllerContext = controllerContext
+        };
+
+        // Act
+        var result = await controller.SendNotification(senderType: notificationType,
+             messageTo: "messageTo",
+             messageBody: "messageBody",
+             messageSubject: "messageSubject");
+
+        // Assert
+        result.Should().BeOfType<OkResult>();
+    }
+
+    [Theory]
+    [InlineData(NotificationType.Email_SMTP)]
+    [InlineData(NotificationType.Email_O365)]
+    [InlineData(NotificationType.SMS)]
+    [InlineData(NotificationType.WhatsApp)]
+    public async Task SendNotification_ShouldReturnOk_IfOnlyMandatoryParams(NotificationType notificationType)
+    {
+        // Arrange
+        var request = MockHttpRequest.GetMock(host, pathBase);
+
+        var context = Mock.Of<HttpContext>(_ =>
+            _.Request == request.Object
+        );
+
+        var controllerContext = new ControllerContext()
+        {
+            HttpContext = context
+        };
+
+        var logger = new Mock<ILogger<NotificationController>>();
+        var notificationHub = new Mock<INotificationHub>();
+
+        var controller = new NotificationController(logger: logger.Object,
+            notificationHub: notificationHub.Object)
+        {
+            ControllerContext = controllerContext
+        };
+
+        // Act
+        var result = await controller.SendNotification(senderType: notificationType,
+             messageTo: "messageTo",
+             messageBody: "messageBody");
+
+        // Assert
+        result.Should().BeOfType<OkResult>();
+    }
+
+}
---
+++ test/NotificationHub.API.Test/Mocks/MockHttpRequest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace NotificationHub.API.Test.Mocks;
+
+internal static class MockHttpRequest
+{
+    internal static Mock<HttpRequest> GetMock(string host, string pathBase)
+    {
+        var request = new Mock<HttpRequest>();
+        request
+            .Setup(x => x.Scheme)
+            .Returns("http");
+        request
+            .Setup(x => x.Host)
+            .Returns(HostString.FromUriComponent(host));
+        request
+            .Setup(x => x.PathBase)
+            .Returns(PathString.FromUriComponent(pathBase));
+
+        return request;
+    }
+}
