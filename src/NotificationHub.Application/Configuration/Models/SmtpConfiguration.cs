f4ac3255be23839903453cfa21ced202d413d9e3
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-02-15 13:33:31 +0100

    test: Add unit test for EmailSmtpSender

--- src/NotificationHub.Application/Configuration/Models/SmtpConfiguration.cs
+++ src/NotificationHub.Application/Configuration/Models/SmtpConfiguration.cs
@@ -4,43 +4,26 @@ namespace NotificationHub.Application.Configuration.Models;
 
 public class SmtpConfiguration
 {
-    private readonly string emailFrom;
-    private readonly string user;
-    private readonly string password;
-    private readonly string server;
-    private readonly int serverPort;
-    private readonly bool enableSSL;
-    private readonly bool enableHtmlBody;
+    public string? EmailFrom { get; set; }
+    public string? User { get; set; }
+    public string? Password { get; set; }
+    public string? Server { get; set; }
+    public int ServerPort { get; set; }
+    public bool EnableSSL { get; set; }
+    public bool EnableHtmlBody { get; set; }
 
-    public string EmailFrom { get { return emailFrom; } }
-    public string User { get { return user; } }
-    public string Password { get { return password; } }
-    public string Server { get { return server; } }
-    public int ServerPort { get { return serverPort; } }
-    public bool EnableSSL { get { return enableSSL; } }
-    public bool EnableHtmlBody { get { return enableHtmlBody; } }
-
-    public SmtpConfiguration()
-    {
-        emailFrom = "";
-        user = "";
-        password = "";
-        server = "";
-        serverPort = 0;
-        enableSSL = false;
-        enableHtmlBody = true;
-    }
 }
 
-internal class SmtpConfigurationValidator
+public class SmtpConfigurationValidator
     : AbstractValidator<SmtpConfiguration>
 {
-    internal SmtpConfigurationValidator()
+    public SmtpConfigurationValidator()
     {
         RuleFor(r => r.EmailFrom)
             .NotNull()
             .NotEmpty()
-            .WithMessage("EmailFrom can not be null or empty");
+            .WithMessage("EmailFrom can not be null or empty")
+            .EmailAddress().WithMessage("A valid email is required");
 
         RuleFor(r => r.User)
             .NotNull()
--- src/NotificationHub.Application/Senders/EmailSmtpSender.cs
+++ src/NotificationHub.Application/Senders/EmailSmtpSender.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.Logging;
 using NotificationHub.Application.Configuration.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Net.Mail;
 
@@ -11,7 +12,7 @@ public class EmailSmtpSender : ISender
     private readonly SmtpConfiguration _config;
     private readonly ILogger _logger;
 
-    internal EmailSmtpSender(SmtpConfiguration configuration, ILogger logger)
+    public EmailSmtpSender(SmtpConfiguration configuration, ILogger logger)
     {
         _config = configuration
             ?? throw new ArgumentNullException(nameof(configuration));
@@ -28,11 +29,24 @@ public class EmailSmtpSender : ISender
             SmtpConfigurationValidator validator = new SmtpConfigurationValidator();
             validator.ValidateAndThrow(_config);
 
+            // Params validations
+            if (string.IsNullOrWhiteSpace(to))
+                throw new FluentValidation.ValidationException("To must not be null or empty");
+            else
+            {
+                var mailValidator = new EmailAddressAttribute();
+                if (!mailValidator.IsValid(to))
+                    throw new FluentValidation.ValidationException("To must be a valid email");
+            }
+
+            if (string.IsNullOrWhiteSpace(body))
+                throw new FluentValidation.ValidationException("Body must not be null or empty");
+
             // Message
             var mailMessage = new MailMessage
             {
                 IsBodyHtml = _config.EnableHtmlBody,
-                From = new MailAddress(_config.EmailFrom),
+                From = new MailAddress(_config.EmailFrom!),
                 Subject = subject,
                 Body = body,
             };
@@ -51,15 +65,20 @@ public class EmailSmtpSender : ISender
                 mailMessage.Dispose();
             };
 
-            await Task.Run(() =>
+            // Send
+            var result = Task.Run(() =>
             {
-                // Manage cancelled
-                if (cancellationToken.IsCancellationRequested)
-                    Task.FromCanceled(cancellationToken);
-
-                // Send
                 smtpClient.Send(mailMessage);
-            });
+            }, cancellationToken);
+
+            if (result.IsCanceled)
+                await Task.FromCanceled(cancellationToken).ConfigureAwait(false);
+
+        }
+        catch (OperationCanceledException cancelled)
+        {
+            _logger.LogError(cancelled.Message);
+            await Task.FromException(cancelled);
         }
         catch (Exception ex)
         {
