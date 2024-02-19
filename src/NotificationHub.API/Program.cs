cd8d2d244453b07d88139327c9cf4868e8ceb460
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-02-19 14:14:13 +0100

    test: change yaml

--- src/NotificationHub.API/Program.cs
+++ src/NotificationHub.API/Program.cs
@@ -10,20 +10,22 @@ namespace NotificationHub.API
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddAPILayer();
+            builder.Services.AddEndpointsApiExplorer()
+                .AddSwaggerGen();
 
+            builder.Services.AddAPILayer();
             builder.Services.AddApplicationLayer();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.MapGroup("/api").WithOpenApi();
+            if (app.Environment.IsDevelopment()) _ = app.UseSwagger().UseSwaggerUI();
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
