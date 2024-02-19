1de30a6d9154f3216352ed13e77425e574601459
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-02-19 15:08:37 +0100

    refact: Change Swagger

--- src/NotificationHub.API/Program.cs
+++ src/NotificationHub.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace NotificationHub.API
 {
     public static class Program
@@ -10,8 +12,21 @@ namespace NotificationHub.API
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer()
-                .AddSwaggerGen();
+            //builder.Services.AddEndpointsApiExplorer()
+            //    .AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.IgnoreObsoleteActions();
+                c.IgnoreObsoleteProperties();
+                c.CustomSchemaIds(type => type.FullName);
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API - NotificationHub",
+                    Version = "v1",
+                    Description = "Cosas de overview..."
+                });
+            });
 
             builder.Services.AddAPILayer();
             builder.Services.AddApplicationLayer();
@@ -19,8 +34,16 @@ namespace NotificationHub.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            app.MapGroup("/api").WithOpenApi();
-            if (app.Environment.IsDevelopment()) _ = app.UseSwagger().UseSwaggerUI();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
+            }
+            //app.MapGroup("/api").WithOpenApi();
+            //if (app.Environment.IsDevelopment()) _ = app.UseSwagger().UseSwaggerUI();
             //if (app.Environment.IsDevelopment())
             //{
             //    app.UseSwagger();
