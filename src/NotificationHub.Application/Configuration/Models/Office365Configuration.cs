43c4c53375547582edd1e5ae19c3063b2b8f1702
Alejandro Blasco <alejandro.blasco@pronet-ise.com>
2024-02-20 09:23:27 +0100

    test: Add unit tests for EmailOffice365 and Config

--- src/NotificationHub.Application/Configuration/Models/Office365Configuration.cs
+++ src/NotificationHub.Application/Configuration/Models/Office365Configuration.cs
@@ -4,11 +4,58 @@ namespace NotificationHub.Application.Configuration.Models;
 
 public class Office365Configuration
 {
+    public string? AuthClientId { get; set; }
+    public string? AuthTenantId { get; set; }
+    public string? AuthClientSecret { get; set; }
 
+    public string? AuthorityUrlBase { get; set; }
+
+    public string? GraphClientSecret { get; set; }
+    public string? GraphApiEndpoint { get; set; }
+
+    public string? EmailFrom { get; set; }
 }
 
-internal class Office365ConfigurationValidator
+public class Office365ConfigurationValidator
     : AbstractValidator<Office365Configuration>
 {
-    internal Office365ConfigurationValidator() { }
+    public Office365ConfigurationValidator()
+    {
+        RuleFor(r => r.EmailFrom)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("EmailFrom can not be null or empty")
+            .EmailAddress().WithMessage("A valid email is required");
+
+        RuleFor(r => r.AuthClientId)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("AuthClientId can not be null or empty");
+
+        RuleFor(r => r.AuthTenantId)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("AuthTenantId can not be null or empty");
+
+        RuleFor(r => r.AuthClientSecret)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("AuthClientSecret can not be null or empty");
+
+        RuleFor(r => r.AuthorityUrlBase)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("AuthorityUrlBase can not be null or empty");
+
+        RuleFor(r => r.GraphClientSecret)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("GraphClientSecret can not be null or empty");
+
+        RuleFor(r => r.EmailFrom)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("EmailFrom can not be null or empty");
+
+    }
 }
---
+++ test/NotificationHub.Application.Test/Sender/EmailOffice365SenderTests.cs
@@ -0,0 +1,237 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NotificationHub.Application.Configuration.Models;
+using NotificationHub.Application.Senders;
+using System.Text.Json;
+
+namespace NotificationHub.Application.Test.Sender;
+
+public class EmailOffice365SenderTests
+{
+    private readonly Mock<ILogger> _logger;
+
+    public EmailOffice365SenderTests()
+    {
+        _logger = new Mock<ILogger>(MockBehavior.Loose);
+    }
+
+    [Fact(Skip = "Do not run until params can be securetly stored")]
+    public async Task Send_ShouldReturnOk_IfAllParamsCorrect()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}";
+        Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().NotThrowAsync<Exception>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfToIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}";
+        Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfToIsNotEmail()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}";
+        Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfBodyIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfAuthTenantIdIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfAuthClientSecretIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfAuthorityUrlBaseIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfGraphClientSecretIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfGraphApiEndpointIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<System.ArgumentException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfGraphApiEndpointIsNotUriEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom@test.com\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<System.ArgumentException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfEmailFromIsEmpty()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldThrowValidationException_IfEmailIsNotEmail()
+    {
+        // Arrange
+        var jsonConfig = "{\"AuthClientId\": \"AuthClientId\",\"AuthTenantId\": \"AuthTenantId\",\"AuthClientSecret\": \"AuthClientSecret\",\"AuthorityUrlBase\": \"AuthorityUrlBase\",\"GraphClientSecret\": \"GraphClientSecret\",\"GraphApiEndpoint\": \"GraphApiEndpoint\",\"EmailFrom\": \"EmailFrom\"}"; Office365Configuration? configuration = JsonSerializer.Deserialize<Office365Configuration>(jsonConfig);
+
+        Senders.ISender sender = new EmailOffice365Sender(configuration!, _logger.Object);
+
+        // Act
+        var act = async () =>
+        {
+            await sender.SendAsync("to@to.com", "subject", "body", CancellationToken.None);
+        };
+
+        // Assert
+        await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+    }
+}
